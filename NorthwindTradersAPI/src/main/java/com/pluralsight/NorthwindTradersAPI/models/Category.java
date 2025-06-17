a877ee94541bf16e99ee809ba6c50fe645a3d883
arthurmov <officialamovsesyan@gmail.com>
2025-06-17 13:03:12 -0400

    before lunch

---
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/models/Category.java
@@ -0,0 +1,39 @@
+package com.pluralsight.NorthwindTradersAPI.models;
+
+public class Category {
+    private int categoryId;
+    private String categoryName;
+
+    public Category(){
+        //do nothing
+    }
+
+    public Category(int categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", categoryName='" + categoryName + '\'' +
+                '}';
+    }
+}
